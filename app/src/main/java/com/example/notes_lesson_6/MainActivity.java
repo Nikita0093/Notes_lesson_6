39ba1f8f1066732ceeece4359863b65838e7d831
Nikita Dyachenko <nikita.dyachenko93@bk.ru>
2022-02-19 23:18:23 +0300

    Урок 6 - Доделали урок

--- app/src/main/java/com/example/notes_lesson_6/MainActivity.java
+++ app/src/main/java/com/example/notes_lesson_6/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.notes_lesson_6;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Notes currentNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +17,16 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState == null) {
             Notes_Names notes_names = Notes_Names.newInstance();
             getSupportFragmentManager().beginTransaction().replace(R.id.Names, notes_names).commit();
-            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                Notes defaultName = new Notes(0);
-                NotesDescription notes_Description = NotesDescription.newInstance(defaultName);
-                getSupportFragmentManager().beginTransaction().replace(R.id.Description, notes_Description).commit();
+        }
+    }
 
-            }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Fragment backStackFragment = (Fragment) getSupportFragmentManager().findFragmentById(R.id.Names);
+        if (backStackFragment != null && backStackFragment instanceof NotesDescription) {
+            onBackPressed();
         }
     }
 }
+
--- app/src/main/java/com/example/notes_lesson_6/Notes_Names.java
+++ app/src/main/java/com/example/notes_lesson_6/Notes_Names.java
@@ -15,6 +15,9 @@ import android.widget.TextView;
 
 
 public class Notes_Names extends Fragment {
+    public static final String CURRENT_NOTE = "Current_note";
+    private Notes currentNote;
+
     public static Notes_Names newInstance() {
         Notes_Names fragment = new Notes_Names();
 
@@ -26,38 +29,60 @@ public class Notes_Names extends Fragment {
         return inflater.inflate(R.layout.fragment_notes__names, container, false);
     }
 
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable(CURRENT_NOTE, currentNote);
+
+    }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        if (savedInstanceState != null) {
+            currentNote = savedInstanceState.getParcelable(CURRENT_NOTE);
+        } else {
+            currentNote = new Notes(0);
+        }
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            showLand();
+
+        }
+        initView((LinearLayout) view);
+    }
+
+
+    private void initView(LinearLayout view) {
         String[] noteNames = getResources().getStringArray(R.array.Notes_Names);
         for (int a = 0; a < noteNames.length; a++) {
             String noteName = noteNames[a];
             TextView textView = new TextView(getContext());
             textView.setText(noteName);
             textView.setTextSize(30f);
-            ((LinearLayout) view).addView(textView);
+            view.addView(textView);
             final int finalA = a;
             textView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Notes Name = new Notes(finalA);
+                    currentNote = new Notes(finalA);
                     if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                        NotesDescription notes_Description = NotesDescription.newInstance(Name);
+                        showLand();
 
-                        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.text_description,notes_Description).commit();
-
-                    }else {
-                        NotesDescription notes_Description = NotesDescription.newInstance(Name);
-
-                        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.Names,notes_Description).addToBackStack(" ").commit();
-
-
-
+                    } else {
+                        showPortrait();
                     }
                 }
             });
-
         }
+    }
+
+    private void showPortrait() {
+        NotesDescription notes_Description = NotesDescription.newInstance(currentNote);
+        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.Names, notes_Description).addToBackStack(" ").commit();
+    }
+
+    private void showLand() {
+        NotesDescription notes_Description = NotesDescription.newInstance(currentNote);
+        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.Description, notes_Description).commit();
     }
 }
