60259069e769e43f30d1eae652fc91127ba208ec
Nikita Dyachenko <nikita.dyachenko93@bk.ru>
2022-02-25 16:40:23 +0300

    Урок 8 - Сделал меню во Фрагменте

--- app/src/main/java/com/example/notes_lesson_6/NotesDescriptionChild.java
+++ app/src/main/java/com/example/notes_lesson_6/NotesDescriptionChild.java
@@ -2,9 +2,13 @@ package com.example.notes_lesson_6;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -23,6 +27,23 @@ public class NotesDescriptionChild extends Fragment {
     }
 
     @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.descriptoin_child_menu, menu);
+        menu.findItem(R.id.action_about).setVisible(false);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case (R.id.action_Share): {
+                Toast.makeText(requireContext(), "Sharing", Toast.LENGTH_LONG).show();
+                break;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_notes_descriptionchild, container, false);
     }
@@ -30,6 +51,7 @@ public class NotesDescriptionChild extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        setHasOptionsMenu(true);
         notes = getArguments().getParcelable(Args_Note_Names);
         TextView textView = view.findViewById(R.id.text_description);
         String[] Description = getResources().getStringArray(R.array.Notes_Description);
